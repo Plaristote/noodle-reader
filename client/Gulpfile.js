6ff90979b9a5841395e3a44eb8c4ac5ab849e8c2
Michael Martin Moro <sexymimi64@gmail.com>
2015-01-15 17:05:44 +0000

    create the client

---
+++ client/Gulpfile.js
@@ -0,0 +1,48 @@
+var gulp           = require('gulp');
+var bower          = require('gulp-bower');
+var gulpBowerFiles = require('gulp-bower-files');
+var concat         = require('gulp-concat');
+var coffee         = require('gulp-coffee');
+var uglify         = require('gulp-uglify');
+var sass           = require('gulp-sass');
+
+var debug = true;
+
+var paths = {
+  js:     [ 'app/javascripts/vendor/**/*.js', 'app/javascripts/**/*.js' ],
+  sass:   [ 'app/stylesheets/application.sass', 'app/stylesheets/**/*.css' ],
+  coffee: [ 'app/coffee/**/*.coffee' ]
+};
+
+gulp.task('bower-files', ['bower'], function() {
+  return gulpBowerFiles().pipe(gulp.dest("./app/javascripts/vendor"));
+});
+
+gulp.task('bower', function() {
+  return bower();
+});
+
+gulp.task('coffee', function() {
+  return gulp.src(paths.coffee).pipe(coffee()).pipe(concat('application.js')).pipe(gulp.dest("./app/javascripts/"));
+});
+
+gulp.task('javascript', ['coffee'], function() {
+  var js = gulp.src(paths.js).pipe(concat('application.js'));
+
+  if (debug == false)
+    js = js.pipe(uglify());
+  return js.pipe(gulp.dest("../server/public/javascripts/"));
+});
+
+gulp.task('stylesheets', function() {
+  return gulp.src(paths.sass).pipe(sass()).pipe(concat('application.css')).pipe(gulp.dest("../server/public/stylesheets/"));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./bower.json', ['bower-files']);
+  gulp.watch(paths.js,     ['javascript']);
+  gulp.watch(paths.sass,   ['stylesheets']);
+  gulp.watch(paths.coffee, ['coffee']);
+});
+
+gulp.task('default', ['bower-files', 'watch']);
